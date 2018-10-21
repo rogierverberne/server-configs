385130300a9cdde0df7964497a106b84867a6ba1
Rogier <verberne.rogier@gmail.com>
2018-10-21 21:11:16 +0200

    Added kodi

--- run.ps1
+++ run.ps1
@@ -1,3 +1,3 @@
 docker rm -f ansible-control
-docker build . -t ansible-control
+docker build .\ansible-client -t ansible-control
 docker run -it --name ansible-control --hostname control  -v $pwd/playbooks/:/playbooks -v $pwd/credentials:/credentials ansible-control /bin/bash
